ebd9ab31e7361f7220277e36c15613ae7ee2df22
johanCas320 <61796945+johanCas320@users.noreply.github.com>
2020-06-27 19:45:51 -0500

    Update captura_de_datos_downsampler.v

--- src/project_1.srcs/sources_1/new/captura_de_datos_downsampler.v
+++ src/project_1.srcs/sources_1/new/captura_de_datos_downsampler.v
@@ -20,57 +20,56 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module captura_de_datos_downsampler(
-input PCLK,
-input HREF,
-input VSYNC,
-input D0,
-input D1,
-input D2,
-input D3,
-input D4,
-input D5,
-input D6,
-input D7,
-output reg [11:0] DP_RAM_data_in,
-output reg [16:0] DP_RAM_addr_in,
-output reg DP_RAM_regW
-    );
-    reg cont=1'b0;
-    reg [7:0] color;
-    
-    always@(posedge PCLK)
+module captura_datos_doadsgfasdgwnsaasdfmpler (PCLK, HREF, VSYNC, D0, D1, D2, D3, D4, D5, D6, D7, DP_RAM_regW, DP_RAM_addr_in, DP_RAM_data_in);
+  
+  localparam AW=15;
+  localparam DW=12;
+  
+  input PCLK, HREF, VSYNC, D0, D1, D2, D3, D4, D5, D6, D7;
+  input [7:0] D;
+  
+  output wire DP_RAM_regW;
+  output wire [AW-1:0] DP_RAM_addr_in;
+  output reg [DW-1:0] DP_RAM_data_in;
+  
+  reg [1:0] cont = 1`b0;
+  reg [7:0] color;
+  
+  initial
     begin
-        if(HREF & ~VSYNC)
+      always @ (posedge pclk)
         begin
-            color[0] = D0;
-            color[0] = D1;
-            color[0] = D2;
-            color[0] = D3;
-            color[0] = D4;
-            color[0] = D5;
-            color[0] = D6;
-            color[0] = D7;
-            if (cont==0)
+          if(HREF & ~VSYNC)
             begin
-                DP_RAM_data_in <= {color[3:0],DP_RAM_data_in[7:0]};
-                DP_RAM_regW =0;
-            end
-            else
+              color [0] = D0;
+          	  color [1] = D1;
+          	  color [2] = D2;
+          	  color [3] = D3;
+         	  color [4] = D4;
+        	  color [5] = D5;
+        	  color [6] = D6;
+              color [7] = D7;
+              if(cont == 0)
+                begin
+                  DP_RAM_data_in <= {color[3:0], DP_RAM_data_in[7:0]};
+              	  DP_RAM_regW = 0;
+                end
+              else
+            	begin
+                  DP_RAM_data_in <= {DP_RAM_data_in[11:8], color[7:0]};
+                  DP_RAM_regW = 1;
+            	end
+          	  cont = cont + 1;
+        	end
+       	end
+      always @ (negedge pclk)
+        begin
+          if(HREF & ~VSYNC & (cont == 1)
             begin
-                DP_RAM_data_in <= {DP_RAM_data_in[11:8],color[7:0]};
-                DP_RAM_regW =1;
+              DP_RAM_addr_in = DP_RAM_addr_in + 1; 
             end
-            cont = cont+1;
+             if (DP_RAM_addr_in == 2**AW)
+               DP_RAM_addr_in = 0;
         end
     end
-    always@(negedge PCLK)
-    begin
-        if(HREF & ~VSYNC & (cont==1))
-        begin
-            DP_RAM_addr_in=DP_RAM_addr_in+1;
-        end
-        if(DP_RAM_addr_in == 76800)
-            DP_RAM_addr_in = 0;
-        end
 endmodule
