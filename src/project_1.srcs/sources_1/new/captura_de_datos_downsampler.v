21087444bdf268e1d3ed0c49d55984286b819611
johanCas320 <61796945+johanCas320@users.noreply.github.com>
2020-07-05 13:30:40 -0500

    Update captura_de_datos_downsampler.v

--- src/project_1.srcs/sources_1/new/captura_de_datos_downsampler.v
+++ src/project_1.srcs/sources_1/new/captura_de_datos_downsampler.v
@@ -82,3 +82,49 @@ module captura_datos_downsampler #(
         end
     end
 endmodule
+//`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:14:22 12/02/2019 
+// Design Name: 
+// Module Name:    cam_read 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+//module cam_read #(
+//		parameter AW = 15; // Cantidad de bits  de la dirección 
+//		parameter DW = 12
+//		)(
+//		input pclk,
+//		input rst,
+//		input vsync,
+//		input href,
+//		input [DW-1:0] px_data,
+//
+//		output [AW-1:0] mem_px_addr,
+//		output [DW-1:0]  mem_px_data,
+//		output px_wr
+//   );
+//	
+//
+/********************************************************************************
+
+Por favor colocar en este archivo el desarrollo realizado por el grupo para la 
+captura de datos de la camara 
+
+debe tener en cuenta el nombre de las entradas  y salidad propuestas 
+
+********************************************************************************/
+
+//endmodule
