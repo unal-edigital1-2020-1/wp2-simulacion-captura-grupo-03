4b14a6bd812b1ae24e82ff7aa4606707b417cff3
johanCas320 <61796945+johanCas320@users.noreply.github.com>
2020-07-20 05:28:37 -0500

    Update test_cam_TB.v

--- src/simulation/test_cam_TB.v
+++ src/simulation/test_cam_TB.v
@@ -112,20 +112,17 @@ localparam d="D:/UNAL/semester6/digitali/proyecto/wp2-simulacion-captura-grupo-0
 	parameter TAM_ROW=120;
 	parameter BLACK_TAM_LINE=4;
 	parameter BLACK_TAM_ROW=4;
-	/*//simulacion de color(propuesta 2)
-	//registros de simulacion del color
-    reg cont=0;
-    parameter[3:0]R=4'b0000; //rojo del pixel RRRR
-    parameter[3:0]G=4'b0000; //verde del pixel GGGG
-    parameter[3:0]B=4'b0000; //azul del pixel BBBB
-    reg [11:0]colorRGB444= {R[3:0],G[3:0],B[3:0]}; //color RRRR GGGG BBBB,first byte= XXXX RRRR, second byte= GGGG BBBB
-	*/
+	
 	/*************************************************************************
 			INICIO DE SIMULACION DE SE�ALES DE LA CAMARA
 	**************************************************************************/
-	/*
-	//simulacion del color(propuesta 2)
-	// color =firstByte XXXX RRRR _ Second BYTE GGGG BBBB;
+	/*//simulacion de color(propuesta 2)
+	//registros de simulacion del color
+    	reg cont=0;
+    	parameter[3:0]R=4'b0000; //rojo del pixel RRRR
+    	parameter[3:0]G=4'b0000; //verde del pixel GGGG
+    	parameter[3:0]B=4'b0000; //azul del pixel BBBB
+    	reg [11:0]colorRGB444= {R[3:0],G[3:0],B[3:0]}; //color RRRR GGGG BBBB,first byte= XXXX RRRR, second byte= GGGG BBBB
 	always @(posedge pclk) begin
 	cont=cont+1;
 	if (cont ==0)begin//first Byte
