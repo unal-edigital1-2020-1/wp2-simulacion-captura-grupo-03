19517fb6e3ff18ccc8a07f8417df5eadb43c019e
nacaceresp <42379708+nacaceresp@users.noreply.github.com>
2020-07-27 02:49:26 -0500

    fghfhjk

--- src/simulation/test_cam_TB.v
+++ src/simulation/test_cam_TB.v
@@ -55,7 +55,11 @@ module test_cam_TB;
 
     // Senales de prueba ******************************
 // Absolute Address in Esteban's computer
-localparam d="D:/UNAL/semester6/digitali/proyecto/wp2-simulacion-captura-grupo-03/src/test_vga.txt";
+	// Absolute address in Esteban's computer
+//localparam d="D:/UNAL/semester6/digitali/proyecto/wp2-simulacion-captura-grupo-03/src/test_vga.txt";
+	// Absolute address in Niko's computer
+localparam d="C:/Users/LucasTheKitten/Desktop/Captura/wp2-simulacion-captura-grupo-03/src/test_vga.txt";
+
 	// Instantiate the Unit Under Test (UUT)
 	test_cam uut (
 		.clk(clk),
